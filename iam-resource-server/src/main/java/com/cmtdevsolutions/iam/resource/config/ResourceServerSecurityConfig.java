95dc7f5f399f87894f9ce2778141eb4c16c91e7d
Cheyernex Manzanillo <cheyernex@gmail.com>
2026-09-21 11:07:25 -0400

    fix(resource-server): TenantContextFilter after BearerTokenAuthenticationFilter para 401->200 en /usuarios y /plantillas

--- iam-resource-server/src/main/java/com/cmtdevsolutions/iam/resource/config/ResourceServerSecurityConfig.java
+++ iam-resource-server/src/main/java/com/cmtdevsolutions/iam/resource/config/ResourceServerSecurityConfig.java
@@ -8,8 +8,8 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.web.authentication.BearerTokenAuthenticationFilter;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -31,7 +31,7 @@ public class ResourceServerSecurityConfig {
                         .anyRequest().authenticated()
                 )
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(jwtAuthenticationConverter())))
-                .addFilterBefore(tenantContextFilter, UsernamePasswordAuthenticationFilter.class);
+                .addFilterAfter(tenantContextFilter, BearerTokenAuthenticationFilter.class);
 
         return http.build();
     }
